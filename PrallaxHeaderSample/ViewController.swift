b1a55c6965ee37dc2168e941a14bf51b0e085595
Yuichiro Takahashi <ikmy0709@gmail.com>
2020-05-24 13:26:50 +0900

    Initial Commit

---
+++ PrallaxHeaderSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrallaxHeaderSample
+//
+//  Created by Yuichirou Takahashi on 2020/05/24.
+//  Copyright © 2020 chanuapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
